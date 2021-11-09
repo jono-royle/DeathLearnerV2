10f644f659868433097c4fe7a1f5754ca1283770
jono-royle <jono.royle@gmail.com>
2021-11-09 09:26:15 +0100

    Add a simple powershell script installer

---
+++ DLInstaller.ps1
@@ -0,0 +1,5 @@
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$assetsDir  = Join-Path -Path $scriptDir -ChildPath \Assets\MLConsoleApp
+$dataDir  = Join-Path -Path $scriptDir -ChildPath \CompletedGame\DeathLearner_Data\MLConsoleApp
+Get-ChildItem "$assetsDir" -Filter *.zip | Expand-Archive -DestinationPath "$assetsDir" -Force
+Get-ChildItem "$dataDir" -Filter *.zip | Expand-Archive -DestinationPath "$dataDir" -Force
